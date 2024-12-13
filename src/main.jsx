59d8968c6635ad0edb35c02e811609e409beb318
khalid-tourhzaoui <tarikkhalidoss100v3356@gmail.com>
2024-12-13 15:19:04 +0000

    React-routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import SignInPage from './auth/sign-in/index.jsx'
+import Home from './Home/Home';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router=createBrowserRouter([
+  {
+    path:'/',
+    element:<Home/>
+  },
+  {
+    path:'/auth/sign-in',
+    element:<SignInPage/>
+  },
+  
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <RouterProvider router={router} />
+  </React.StrictMode>,
 )
